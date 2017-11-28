50b33400f77e6ca156cd1dc8c2ccbb9690ff1ffa
9o9b <33761428+9o9b@users.noreply.github.com>
2017-11-28 09:43:37 -0800

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,23 @@
+print("hello world")
+
+# this a new line
+
+# Jalen
+
+a = 4
+b = 3
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
+
+print
+print("try to figure out how this work")
+print(13 % 5)
+
+# the"%" sign is a modlus it finds the remainder
+
+car_name = "wiebe mobile"
+car_type = "BMW"
