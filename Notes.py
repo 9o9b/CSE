a0e59965a2d1b06908dcd4fb17ba9d177c0a0083
9o9b <33761428+9o9b@users.noreply.github.com>
2017-11-29 09:46:19 -0800

    Added Functions
    11-29-17

--- Notes.py
+++ Notes.py
@@ -1,23 +1,80 @@
-print("hello world")
+# print("hello world")
+#
+# # this a new line
+#
+# # Jalen
+#
+# a = 4
+# b = 3
+#
+# print(3 + 5)
+# print(5 - 3)
+# print(3 * 5)
+# print(6 / 2)
+# print(3 ** 2)
+#
+# print
+# print("try to figure out how this work")
+# print(13 % 5)
+#
+# # the"%" sign is a modlus it finds the remainder
+#
+# car_name = "wiebe mobile"
+# car_type = "BMW"
 
-# this a new line
 
-# Jalen
 
-a = 4
-b = 3
 
-print(3 + 5)
-print(5 - 3)
-print(3 * 5)
-print(6 / 2)
-print(3 ** 2)
 
-print
-print("try to figure out how this work")
-print(13 % 5)
 
-# the"%" sign is a modlus it finds the remainder
 
-car_name = "wiebe mobile"
-car_type = "BMW"
+
+def print_hw():
+    print("Hello world")
+    print("Enjoy the day.")
+
+print_hw()
+
+
+def say_hi(name):   # Name is a "parameter
+    print("hello %s" % name)
+    print("coding is great!")
+
+say_hi("Jalen")
+
+
+def print_age(name, age):
+    print("%s is %d years old" % (name, age))
+    age = age + 1 # age = age + 1
+    print("Next year, %s will be %d years old %" % (name, age))
+
+
+print_age("Jalen", 15)
+
+
+def algebra_hw(x):
+   return x**3 + 4*x**2 + 7 * x - 4
+
+print(algebra_hw(3))
+print(algebra_hw(4))
+print(algebra_hw(5))
+print(algebra_hw(6))
+print(algebra_hw(7))
+
+
+# if statements
+
+
+def grade_calc(percentage):
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 80 and percentage < 90:  # elif
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    else:
+        return "F"
+
+    grade_calc(92)
