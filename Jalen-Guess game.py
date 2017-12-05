23aef73b92405366daa8764aee7941c7bcd0e619
9o9b <33761428+9o9b@users.noreply.github.com>
2017-12-05 09:45:15 -0800

    12.5.17
    input numbers

--- Jalen-Guess game.py
+++ Jalen-Guess game.py
@@ -1,3 +1,10 @@
-# random numbers
-import random # this should be on line 1
+# grnerate a number
+# ask the user for an input (number)
+# does the guess match the number
+# add "higher" and "lower"
+# add 5 guesses
+
+
+import random
 print(random.randint(1, 50))
+import numbers
--- Warmups.py
+++ Warmups.py
@@ -1,2 +1,10 @@
+def first_name(jalen):
+    pass
+
+
 def reverse_order(firstname, lastname):
     print(last_name + " " + first_name) # Concatenation
+
+#12-5-17
+def add_py(name):
+    print(name + ".py")
