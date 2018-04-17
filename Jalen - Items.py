3a752e5a8a0ffb84a645a1d9a8c4731a51de34ac
9o9b <33761428+9o9b@users.noreply.github.com>
2018-04-17 09:00:13 -0700

    fixing items

--- Jalen - Items.py
+++ Jalen - Items.py
@@ -1,6 +1,25 @@
-class items(melee):
-    def __init__(self, sword, bat, axe):
-        self.sword = sword
-        self.bat = bat
-        self.axe = axe
-        self. =
+class Item(object):
+        self.name = name
+
+    def pick_up(self)::
+
+        print("You pick up the %s" % self.name)
+
+
+class Weapon(Item):
+    def __init__(self, name):    def __init__(self, name)
+    def __init__(self, name, damage):
+        super(Weapon, self).__init__(name)
+        self.damage = damage
+
+    def attack(self):
+        print("You attack with the %s" % self.name)
+
+
+bat = Weapon("Bat", 10)
+sword = Weapon("Sword", 10)
+
+class Armour(Item):
+    def _init_(self, name, armour):
+        super(armour, self)._init_(name)
+        self.armour = armour
