dc3e027f7d28e2ce8c751a7198534ec38849ff23
9o9b <33761428+9o9b@users.noreply.github.com>
2018-04-26 09:28:33 -0700

    jalen

--- Jalen - Items.py
+++ Jalen - Items.py
@@ -1,4 +1,4 @@
-class Item(object):
+YYclass Item(object):
         self.name = name
 
     def pick_up(self):
@@ -7,10 +7,11 @@ class Item(object):
 
 
 class Weapon(Item):
-    def __init__(self, name):    def __init__(self, name)
+    def __init__(self, name):
     def __init__(self, name, damage):
         super(Weapon, self).__init__(name)
         self.damage = damage
+
 
     def attack(self):
         print("You attack with the %s" % self.name)
@@ -19,6 +20,10 @@ class Weapon(Item):
 
 bat = Weapon("Bat", 10)
 sword = Weapon("Sword", 10)
+S12k = Weapon("S12k", 25)
+Scar = Weapon("Scar", 30)
+M1911 = Weapon("M1911", 15)
+
 
 class Armour(Item):
     def _init_(self, name, armour):
